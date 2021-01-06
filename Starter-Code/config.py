2ab0303fbf9478da796ed5798c6b424ad25c4434
Sveena Sharma <sveenarsharma@gmail.com>
2021-01-06 18:04:59 -0500

    homework

---
+++ Starter-Code/config.py
@@ -0,0 +1,9 @@
+sf_username = 'sveenarsharma@gmail.com' 
+sf_password = 'Toby101!' 
+sf_security_token = 'LMltB7XyrwyXi9XMHDX8xeAd' 
+
+remote_db_endpoint = 'databootcamp.c6st03plaqmh.us-east-1.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'Toby101!'
+remote_db_name = 'gwsis'
